ee39540b83e8d4c8700dc86f57fd043cc18f7e2a
nick thomas <nichoth@gmail.com>
2023-07-26 11:21:45 -0700

    better ci

--- test/index.browser.ts
+++ test/index.browser.ts
@@ -1,11 +1,15 @@
 import { test } from '@socketsupply/tapzero'
-import { Program } from '@oddjs/odd'
+import { Program, Session } from '@oddjs/odd'
+import { createUsername } from '@ssc-hermes/profile'
 import { example } from '../dist/index.js'
 
 // @ts-ignore
 const wn = self.oddjs
 
 let program:Program
+let alicesUsername:string
+let session:Session|null
+
 test('setup', async t => {
     const APP_INFO = { name: 'testing', creator: 'test' }
     program = await wn.program({
@@ -15,9 +19,37 @@ test('setup', async t => {
 
     t.ok(wn, 'wn should exist')
     t.ok(program, 'should create a program')
+
+    alicesUsername = await createUsername(program.components.crypto)
+
+    await program.auth.register({ username: alicesUsername })
+    session = await program.auth.session()
+    const wnfs = session?.fs
+    if (!wnfs) throw new Error('not wnfs')
+
+    if (!(await program.fileSystem.hasPublicExchangeKey(wnfs))) {
+        await program.fileSystem.addPublicExchangeKey(wnfs)
+    }
+
+    await wnfs.publish()
+    const published = await once(program, 'fileSystem:publish')
+    t.ok(published, 'should emit a publish event')
 })
 
 test('example', t => {
     const random = example(program.components.crypto)
     t.ok(random, 'should return something')
 })
+
+function once (bus:Program, eventName:string) {
+    return new Promise(resolve => {
+        // @ts-ignore
+        bus.on(eventName, onEvent)
+
+        function onEvent (ev) {
+            resolve(ev)
+            // @ts-ignore
+            bus.off(eventName, onEvent)
+        }
+    })
+}
